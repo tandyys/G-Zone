f5ed79d03fbdd06adf81856d12d4970a8aa20b05
tandyys <tansabana12@gmail.com>
2025-02-05 16:51:01 +0700

    Working on home view
    Adding background image for every navigation links

--- G-Zone.swiftpm/ContentView.swift
+++ G-Zone.swiftpm/ContentView.swift
@@ -12,23 +12,23 @@ struct ContentView: View {
                         .scaledToFill()
                         .frame(width: 350, height: 250)
                     
-                    CreateNavigationLink(destination: models[0], planet: "Mercury", color: .gray)
+                    CreateNavigationLink(destination: models[0], planet: "Mercury")
                     
-                    CreateNavigationLink(destination: models[1], planet: "Venus", color: .orange)
+                    CreateNavigationLink(destination: models[1], planet: "Venus")
                     
-                    CreateNavigationLink(destination: models[2], planet: "Earth", color: .gray)
+                    CreateNavigationLink(destination: models[2], planet: "Earth")
                     
-                    CreateNavigationLink(destination: models[3], planet: "Mars", color: .orange)
+                    CreateNavigationLink(destination: models[3], planet: "Mars")
                     
-                    CreateNavigationLink(destination: models[4], planet: "Jupiter", color: .gray)
+                    CreateNavigationLink(destination: models[4], planet: "Jupiter")
                     
-                    CreateNavigationLink(destination: models[5], planet: "Saturn", color: .orange)
+                    CreateNavigationLink(destination: models[5], planet: "Saturn")
                     
-                    CreateNavigationLink(destination: models[6], planet: "Uranus", color: .gray)
+                    CreateNavigationLink(destination: models[6], planet: "Uranus")
                     
-                    CreateNavigationLink(destination: models[7], planet: "Neptune", color: .orange)
+                    CreateNavigationLink(destination: models[7], planet: "Neptune")
                     
-                    CreateNavigationLink(destination: models[8], planet: "Moon", color: .gray)
+                    CreateNavigationLink(destination: models[8], planet: "Moon")
                 }
             }
             .scrollIndicators(.hidden)
@@ -37,16 +37,24 @@ struct ContentView: View {
     }
         
     @ViewBuilder
-    func CreateNavigationLink(destination: String, planet: String, color: Color) -> some View {
+    func CreateNavigationLink(destination: String, planet: String) -> some View {
         NavigationLink(destination: DetailView(scene: .init(named: destination))) {
             Text("Explore " + planet)
                 .font(.system(size: 24, weight: .heavy))
-                .foregroundStyle(.white)
-                .padding()
+                .foregroundStyle(.black)
+                .overlay(
+                    Text("Explore " + planet)
+                        .font(.system(size: 24, weight: .heavy))
+                        .foregroundColor(.white)
+                        .offset(x: 2, y: 2)
+                )
                 .frame(width: 350, height: 70)
                 .background(
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(color)
+                    Image(planet)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 350, height: 70)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
                 )
                 .padding(.top)
         }
