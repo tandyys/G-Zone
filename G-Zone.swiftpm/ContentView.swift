930a40141ea7a0bb81861cfe84e1f4fc4dd7fad0
tandyys <tansabana12@gmail.com>
2025-02-05 16:22:05 +0700

    Working on Home View
    Adding logo and NavigationLinks

--- G-Zone.swiftpm/ContentView.swift
+++ G-Zone.swiftpm/ContentView.swift
@@ -4,31 +4,51 @@ struct ContentView: View {
     let models: [String] = ["Mercury.scn", "Venus.scn", "Earth.scn", "Mars.scn", "Jupiter.scn", "Saturn.scn", "Uranus.scn", "Neptune.scn", "Moon.scn"]
     
     var body: some View {
-        VStack {
-            NavigationStack {
+        NavigationStack {
+            ScrollView {
                 VStack {
+                    Image("logo")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 350, height: 250)
+                    
                     CreateNavigationLink(destination: models[0], planet: "Mercury", color: .gray)
                     
                     CreateNavigationLink(destination: models[1], planet: "Venus", color: .orange)
+                    
+                    CreateNavigationLink(destination: models[2], planet: "Earth", color: .gray)
+                    
+                    CreateNavigationLink(destination: models[3], planet: "Mars", color: .orange)
+                    
+                    CreateNavigationLink(destination: models[4], planet: "Jupiter", color: .gray)
+                    
+                    CreateNavigationLink(destination: models[5], planet: "Saturn", color: .orange)
+                    
+                    CreateNavigationLink(destination: models[6], planet: "Uranus", color: .gray)
+                    
+                    CreateNavigationLink(destination: models[7], planet: "Neptune", color: .orange)
+                    
+                    CreateNavigationLink(destination: models[8], planet: "Moon", color: .gray)
                 }
             }
         }
+        .ignoresSafeArea()
     }
-    
+        
     @ViewBuilder
     func CreateNavigationLink(destination: String, planet: String, color: Color) -> some View {
         NavigationLink(destination: DetailView(scene: .init(named: destination))) {
             Text("Explore " + planet)
-                .font(.headline)
+                .font(.system(size: 24, weight: .heavy))
                 .foregroundStyle(.white)
                 .padding()
-                .frame(width: 250, height: 35)
+                .frame(width: 350, height: 70)
                 .background(
                     RoundedRectangle(cornerRadius: 10)
                         .fill(color)
                 )
+                .padding(.top)
         }
-        .padding()
     }
 }
 
